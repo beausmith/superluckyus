eb86e4ca31635b6d374da3803d0a0eb24d6e6d2e
Beau Smith <beau@beausmith.com>
2016-01-04 12:52:40 +0100

    v1

--- superlucky.js
+++ superlucky.js
@@ -21,69 +21,24 @@ $(document).ready(function() {
     $('.attending-fields').toggle($(this).val() === 'yes');
   });
 
-  $('#test-form').bootstrapValidator({
-    //submitButtons: '#postForm',
-    // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
-    feedbackIcons: {
-      valid: 'glyphicon glyphicon-ok',
-      invalid: 'glyphicon glyphicon-remove',
-      validating: 'glyphicon glyphicon-refresh'
-    },
-    fields: {
-      firstName: {
-      message: 'The first name is not valid',
-        validators: {
-          notEmpty: {
-            message: 'We need to know who you are!'
-          },
-        }
-      },
-      lastName: {
-        message: 'Last Name is not valid',
-        validators: {
-          notEmpty: {
-            message: 'Last Name is required and cannot be empty'
-          },
-        }
-      },
-      email: {
-        validators: {
-          notEmpty: {
-            message: 'The email address is required and cannot be empty'
-          },
-          emailAddress: {
-            message: 'The email address is not a valid'
-          }
-        }
-      },
-      address: {
-        message: 'Address is not valid',
-        validators: {
-          notEmpty: {
-            message: 'Address is required and cannot be empty'
-          }
-        }
-      },
-    }
-  })
-  .on('success.form.bv', function(e) {
-    // Prevent form submission
+  $('[name="speechOtherName"]').bind('keyup', function(){
+    $('[name="speechOther"]').prop('checked', !!$(this).val().trim());
+  });
+
+  $('#rsvp-form').bootstrapValidator({}).on('success.form.bv', function(e) {
     e.preventDefault();
 
     // Get the form instance
     var $form = $(e.target);
 
-    // Get the BootstrapValidator instance
-    var bv = $form.data('bootstrapValidator');
-
     // Use Ajax to submit form data
     var url = 'https://script.google.com/macros/s/AKfycbxjhVrWmr2ho6GJSe-gxNtfN7e_GBY-0TbUBwxp_EUXFUXLZg/exec';
     var redirectUrl = 'success-page.html';
     // show the loading
-    $('#postForm').prepend($('<span></span>').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate'));
+    $('#postForm').prepend($('<span></span>').addClass('glyphicon glyphicon-refresh glyphicon-spin button-validating'));
     var jqxhr = $.post(url, $form.serialize(), function(data) {
-        console.log("Success! Data: " + data.statusText);
-        $(location).attr('href',redirectUrl);
+      // console.log("Success! Data: " + data.statusText);
+      $(location).attr('href',redirectUrl);
     })
     .fail(function(data) {
       console.warn("Error! Data: " + data.statusText);
